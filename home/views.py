e028b48c3c3d77567b7c0e16ca1bb58b25bdb1f9
Jana <jana.llorin@gmail.com>
2021-01-16 19:36:01 -0500

    Updated about.

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 def home_page(request):
     return render(request, 'home/home.html')
+
+def about(request):
+    return render(request, 'home/about.html')
+
+def blog(request):
+    return render(request, 'blog/blog.html')
